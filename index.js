14d7bb7646857d4dc3b1c34ff81d2265f37e9517
DakshataPradhan <pradhandakshata1996@gmail.com>
2022-06-20 14:31:29 -0600

    commit tree 0f73be9664a225a544119827f62e194e06cad41d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names,eventname){
+    let arr = [];
+    for (let i=0;i<names.length; i++){
+        let str = `Thank you, ${names[i]}, for the wonderful ${eventname} gift!`;
+        arr.push(str);
+
+    }
+  return arr;
+}
+function countDown(num){
+    while (num >= 0){
+        console.log(num--);
+    }
+}
+
+
